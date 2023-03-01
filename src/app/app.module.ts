e49b215b5adda5a94745c851047c4f743fde2fa6
HelioAmaral <helio.amaral81@gmail.com>
2023-03-01 11:07:40 +0100

    first changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//to be able to make http requests we need to import httpClientModule
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { PhotoDisplayComponent } from './photo-display/photo-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //add httpClientModule to make http requests
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/photo-fetch.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+//import httpClient (a class) to allow us to make the http request
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoFetchService {
+
+  //Dependency injection to get access to the HttpClient
+  constructor(private http: HttpClient) { }
+
+  //create a method where we write our request to unsplash API
+  getPhoto(){
+    //https://api.unsplash.com/ is the API location and photos/random is the end point from where we want to get our response
+    //we need to add our access key, in this case with a header
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        //headers name and 'Client-ID' comes directly from the documentation and following it is our access key provided when we created a new app in unsplash
+        Authorization: 'Client-ID nI4VUiSMMInjfb_Lhdzpc0onI6kQGV7tGoKZpMkvVKE'
+      }
+    })
+  }
+}
