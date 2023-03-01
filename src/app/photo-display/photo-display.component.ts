327882d561d4c40fb58e9da2d182bde942aa90e5
HelioAmaral <helio.amaral81@gmail.com>
2023-03-01 14:44:33 +0100

    final

--- src/app/photo-display/photo-display.component.ts
+++ src/app/photo-display/photo-display.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import the photo-fetch service to be able to make the http request
+import { PhotoFetchService } from '../photo-fetch.service';
 
 @Component({
   selector: 'app-photo-display',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class PhotoDisplayComponent {
 
+  //property photo will store the result from the http request
+  photo= '';
+
+
+  //make use of dependency injection to access that PhtoFetchService inside the component class
+  constructor(private photoFetchService: PhotoFetchService){
+    //inside the constructor we call the helper function that containes the code for the http request
+this.fetchPhoto();
+  }
+
+  //button 'NEW IMAGE' click, calls the function to get a new random photo
+  onClick(){
+this.fetchPhoto();
+  }
+  
+
+  //this helper function, allow us to write the http request code only once, then we can call this function wherever we need to use the code
+  fetchPhoto(){
+   // we call the getPhoto() from the photo-fetch.service
+  //when we call it, it return to us an observable, the request is not actually issued until we add on a subscribe statement
+  this.photoFetchService.getPhoto().subscribe((response) => {
+    //console.log(response.urls.regular);
+    this.photo = response.urls.regular;
+  });
+
+}
 }
--- src/app/photo-fetch.service.ts
+++ src/app/photo-fetch.service.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@angular/core';
 //import httpClient (a class) to allow us to make the http request
 import { HttpClient } from '@angular/common/http';
 
+
+//we create an interface to specify to TS what we are expecting to get as a response from our request
+//then we apply it as the generic type to the get method bellow, like:
+//return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random ... this will tell TS what type are we getting
+interface UnsplashResponse{
+  urls: {
+    regular: string;
+  }
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,7 +24,7 @@ export class PhotoFetchService {
   getPhoto(){
     //https://api.unsplash.com/ is the API location and photos/random is the end point from where we want to get our response
     //we need to add our access key, in this case with a header
-    return this.http.get('https://api.unsplash.com/photos/random', {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
       headers: {
         //headers name and 'Client-ID' comes directly from the documentation and following it is our access key provided when we created a new app in unsplash
         Authorization: 'Client-ID nI4VUiSMMInjfb_Lhdzpc0onI6kQGV7tGoKZpMkvVKE'
